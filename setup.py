32eec6d7c37c6b473f1687bd377d1bf2a9f630e5
Maximilian Stölzle <maximilian@stoelzle.ch>
2022-01-11 12:18:36 +0100

    Change package name to pygbn

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='py-gbn',  # Required
+    name='pygbn',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -71,7 +71,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/tud-cor-sr/py-gbn',  # Optional
+    url='https://github.com/tud-cor-sr/pygbn',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
@@ -196,7 +196,7 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/tud-cor-sr/py-gbn/issues',
-        'Source': 'https://github.com/tud-cor-sr/py-gbn/',
+        'Bug Reports': 'https://github.com/tud-cor-sr/pygbn/issues',
+        'Source': 'https://github.com/tud-cor-sr/pygbn/',
     },
 )
